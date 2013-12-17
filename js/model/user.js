79d991e2b80288646b696c2a06c709fed706d8e1
Scott Vossen <scottvossen@gmail.com>
2013-12-17 10:06:30 -0600

    Create user data model. (untested)

---
+++ js/model/user.js
@@ -0,0 +1,36 @@
+App.User = DS.Model.extend({
+  // supported types: string, number, boolean, and date
+  id:         DS.attr('number'),
+  firstName:  DS.attr('string'),
+  lastName:   DS.attr('string'),
+  isAwesome:  DS.attr('boolean'),
+  dob:        DS.attr('date'),
+
+  fullName: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName'),
+});
+
+App.User.FIXTURES = [
+ {
+   id:        1,
+   firstName: 'Scott',
+   lastName:  'Vossen',
+   isAwesome: true,
+   dob:       new Date('5-31-1987'),
+ },
+ {
+   id:        2,
+   firstName: 'Issac',
+   lastName:  'Alexander',
+   isAwesome: true,
+   dob:       new Date('11-17-2013'),
+ },
+ {
+   id:        3,
+   firstName: 'Paul',
+   lastName:  'Martin',
+   isAwesome: false,
+   dob:       new Date('10-02-1956'),
+ }
+];

09c1a439ad476b165190d1d1c6c8bea7a8f5dd1d
Scott Vossen <scottvossen@gmail.com>
2013-12-24 12:32:03 -0600

    fail at removing 404 error

--- js/ember-helper.js
+++ js/ember-helper.js
@@ -18,24 +18,39 @@ var EmberHelper = (function() {
 		if (!isPrecompiled) {
 			return templatePath = _templatesDir + fileName + _templateExt;
 		} else {
-			return templatePath = _templatesDir + fileName + _precompiledTemplateExt;
-		}
+			return templatePath = _templatesDir + fileName + _precompiledTemplateExt		};
+
+	}
+
+	function urlExists(url)
+	{
+		// TODO:
+		// Find a way to do this without causing console 404 errors.
+		// Looks like you can't without using serverside js.
+		
+		// NOTE: This is essentially the same as the jquery call in getTemplate anyways..
+	    var http = new XMLHttpRequest();
+	    http.open('HEAD', url, false);
+	    http.send();
+	    return http.status!=404;
 	}
 
 	function getTemplate(templatePath) {
 
 		var template;
 		
-		$.ajax({
-			url: templatePath,
-		    async:false,
-		    success: function(data) {
-		    	template = data;
-		    },
-	      	error: function (xhr, ajaxOptions, thrownError) {
-	      		template = null;
-	      	}
-		 });
+		if (urlExists(templatePath)) {
+			$.ajax({
+				url: templatePath,
+			    async:false,
+			    success: function(data) {
+			    	template = data;
+			    },
+		      	error: function (xhr, ajaxOptions, thrownError) {
+		      		template = null;
+		      	}
+			 });
+		}
 
 		return template;
 	}
